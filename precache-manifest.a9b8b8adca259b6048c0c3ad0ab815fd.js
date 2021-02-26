d47b334d07bb6a1df649629616a9e556b3d25e1f
INSAF <muhammed.insaf@storiyoh.com>
2021-02-26 09:14:58 +0530

    initial commit

---
+++ precache-manifest.a9b8b8adca259b6048c0c3ad0ab815fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b6a53963702e539ba7",
+    "url": "/css/app.075f047f.css"
+  },
+  {
+    "revision": "fcc2ba615f052af1867f1f6bd92da7f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b6a53963702e539ba7",
+    "url": "/js/app.636a5b8a.js"
+  },
+  {
+    "revision": "fa4ed0ecae74523aadb4",
+    "url": "/js/chunk-vendors.e9454aef.js"
+  },
+  {
+    "revision": "aef66d89c470180928b33324b4b7fed4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
